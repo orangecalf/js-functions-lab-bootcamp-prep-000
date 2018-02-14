cdc1adf418fc5566a41fa2f317674ce8f2477242
yossi katzman <issoyk@gmail.com>
2018-02-14 12:00:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays ${name}!`
+  return `Happy holidays, ${name}!`
   }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
